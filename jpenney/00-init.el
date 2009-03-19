4ca2f227a941707078c6531e00a82242f76361a2
Jason Penney <jpenney@mini.local>
2009-03-19 07:37:56 -0400

    darwin tweaks, server startup

--- jpenney/00-init.el
+++ jpenney/00-init.el
@@ -1,3 +1,10 @@
+(require 'server)
+(unless server-process
+  (progn
+    (message "starting server")
+    (server-start)
+))
+
 ;; set up path
 (defvar jcp-home (file-name-directory
                   (or (buffer-file-name) load-file-name)))
